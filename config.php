2f412842655dfdc7e59a0904553ac217df227ab2
xieguanping <695634709@qq.com>
2018-12-16 00:18:50 +0800

    支付demo

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+$config = array (	
+		//应用ID,您的APPID。
+		'app_id' => "2016082000296539",
+
+		//商户私钥，您的原始格式RSA私钥
+		'merchant_private_key' => "MIIEowIBAAKCAQEAsFEE5MMP0+MK2f/+4WFR92DlaP4r/s+Ssx1w8ei7QYYiMusiP4ImqOqTboHwyDIyS72BwaX/fxApI4VUQmkphpRvm7sHaEGXN/SWwtbbovggIIM9TZ3LO8hf43k9ZnaD7TEqSMbzz8afrSq2PKBeVrgFYySSsdN8c4+9OdxkNyV+vMUjLkY0uhbOGt7bO4nl2xfhe8n7MO8ZcAwefXelYM7koyGR8GCoU3cBxhWE/kqQz2bGRtrC5FlL1C9OvPzq+KzwFiaHbz7MxOgldekMUcDXkWmgMCUIOIMQjUpXFPTvHU8bGtfPhtSddbEQPAeCwvu98fexa07+BocIZNy/twIDAQABAoIBAQCusmf93gQFtpUe8RXy7EGRsZE82XJwE+1PyaEKH8XCI4wpFW8mq+dwQJR5zwfq5EmPYCJ74+TmWfXY+xdEgfwiG1sGW2b4dtzFFXR3cvnGXMFAVajdCba/8kaIFa2vo0gOMUOrPm1+9ldp9cMBCQjQNZ8o36j1xw0tV0NvXB5zv+3yVsgRmElZQoIzlo9pWlwbcT2TRqhjFNxzaVtwGJ0vHztm2ZglHqYgKb4gsF3XMNLHJolwsVcay/mSN9B2daqQ808TtyO0RjB30G1dj98UQSWuaFx5qkHiGx2/82QC7dAY2qQnExomKM++XaT2oCeYT8nb7ABRcXQtLOfVSgrhAoGBAOOhxUu4M0kbveK7pwR8cWZS3/MufEwXmcqXjgtn/KvYKOBJkcm6c5WKaDX7hKmozyzDUGEvStNTALGmQe2twp05GqXmqMSryE9INi2rIxiR6joXdvrsDAEZAhUJrQuuNjJfzu05u9983K8yTljesFmi0RPfo39cX90nVv0AzxrnAoGBAMZKHLfYeZOtWEVjcwKpR4JwPUncvTA1s6jRcbg6u2ByhKV7xBYB2gbTKqjqQcMeEqUsAQJLY9Q5AyUwFAsmREMZ9Z1Wrozij5q0SJMvmGkxbhAWdgrbkui1vbAsrQ1u8ipzZXgqy6xh31agN0ME7n0sAxb+VGJCpsVhxYdRzOqxAoGAKgU46ItuXDaUzQAbCsb2IA94a+nO5DUPHMjxXcWqbQaQNScflH+oypgsOJrfmHojFH5Rnj8ervgJjsHENd33qRJbiviTewrDBW+tIK0Q4348INEoE5XzeVhme/+3RAq60n3BMyWPOuhPzL+amukw4LsfoW2NPvuMbsTOWBIIKU0CgYBJbbZ2yj4zVrTidTJo4ODvR7saeBjDGo/eSzkufOlYjO/cDBoQAD2J23bnuGAD7kX/Tm1NOTfNphMpGUXC3zfWFW3kBu56k4eURYC+qJhrE/frEzZBTRB5Bpwo+lJXMCeTWspPlqF3w4KeJJRySAg86Ch4wlPjU0VIwK0SMFOO8QKBgB0q/N1b2LndIcWDb+vD7uUXu51+TdgL5DW0jgwXIKyQFJmD6o80J+yrXnr7AR4J0aHRGlEdzTtQ+gjFMM76tXkAD10rzhdaxrmnv2OMuXdSp6x/iaxJqvUW86QxE6NdvPsAHjRtoeKPSBs+S6/IkdoEznpwHR6pzgxzIIFkvtBG",
+		
+		//异步通知地址
+		'notify_url' => "http://xieguanping.cn",
+		
+		//同步跳转
+		'return_url' => "http://xieguanping.cn",
+
+		//编码格式
+		'charset' => "UTF-8",
+
+		//签名方式
+		'sign_type'=>"RSA2",
+
+		//支付宝网关
+		'gatewayUrl' => "https://openapi.alipaydev.com/gateway.do",
+
+		//支付宝公钥,查看地址：https://openhome.alipay.com/platform/keyManage.htm 对应APPID下的支付宝公钥。
+		'alipay_public_key' => "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAsFEE5MMP0+MK2f/+4WFR92DlaP4r/s+Ssx1w8ei7QYYiMusiP4ImqOqTboHwyDIyS72BwaX/fxApI4VUQmkphpRvm7sHaEGXN/SWwtbbovggIIM9TZ3LO8hf43k9ZnaD7TEqSMbzz8afrSq2PKBeVrgFYySSsdN8c4+9OdxkNyV+vMUjLkY0uhbOGt7bO4nl2xfhe8n7MO8ZcAwefXelYM7koyGR8GCoU3cBxhWE/kqQz2bGRtrC5FlL1C9OvPzq+KzwFiaHbz7MxOgldekMUcDXkWmgMCUIOIMQjUpXFPTvHU8bGtfPhtSddbEQPAeCwvu98fexa07+BocIZNy/twIDAQAB",
+		
+	
+);
